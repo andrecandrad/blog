33e605f012f480461f0ed5fea78847c5c2c3d2eb
andré <andrecandrad@gmail.com>
2022-05-24 00:38:18 -0300

    Criação dos componentes Article e Post

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,28 @@ import Post from '../Post';
 
 const Article = () => {
     return(
-        <>
-            <h1>Article.js</h1>
-            <Post/>
-        </>
+        <section className="container">
+            <h3>Articles</h3>
+            <div className="container-posts mt-5">
+                <Post subtitle="tecnologia" title="O guia definitivo sobre o blog">
+                    Lorem1 ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec sed metus in ligula vestibulum ornare. Suspendisse eu libero 
+                    facilisis arcu consectetur consectetur at quis orci. Duis et libero nulla.
+                </Post>
+
+                <Post subtitle="fotografia" title="Quais as melhores cameras em 2022">
+                    Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec sed metus in ligula vestibulum ornare. Suspendisse eu libero 
+                    facilisis arcu consectetur consectetur at quis orci. Duis et libero nulla.
+                </Post>
+
+                <Post subtitle="cinema" title="Os 10 filmes com maior bilheteria">
+                    Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec sed metus in ligula vestibulum ornare. Suspendisse eu libero 
+                    facilisis arcu consectetur consectetur at quis orci. Duis et libero nulla.
+                </Post>
+            </div>
+        </section>
     )
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,20 @@
-const Post = () => {
+const Post = ({subtitle, title, children}) => { // ao inves de props --> desestruturação, para não ficar chamando props.algumacoisa
     return(
-        <h1>Post.js</h1>
+        <div className="post">
+            <div className="img-post"></div>
+            <h5>{subtitle}</h5>
+            <h3>{title}</h3>
+            <p className="mt-1">
+                {children}
+            </p>
+            <div className="flex pt-2">
+                <div className="img-profile"></div>
+                <div className="dsc-profile ml-2">
+                    <h6 className="color-blue">Fulano de tal</h6>
+                    <p>May 21, 2022 - 8 min read</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
